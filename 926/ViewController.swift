1f514b95cb5bc86ede028cfc36336db407edc367
20161104568 <a20161104568@Mac02.local>
2018-12-26 10:09:23 +0800

    修改计时器
    修改计时器

--- 926/ViewController.swift
+++ 926/ViewController.swift
@@ -8,16 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+ 
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
-    
+    @IBOutlet weak var resetButton: UIButton!
     var counter = 0.0
     var timer = Timer()
     var isPlaying = false
-    
-    @IBAction func startTimer(_ sender: AnyObject) {
+    @IBAction func startTimer(_ sender: Any) {
         if(isPlaying) {
             return
         }
@@ -27,16 +26,14 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
         isPlaying = true
     }
-    
-    @IBAction func pauseTimer(_ sender: AnyObject) {
+    @IBAction func pauseTimer(_ sender: Any) {
         startButton.isEnabled = true
         pauseButton.isEnabled = false
         
         timer.invalidate()
         isPlaying = false
     }
-    
-    @IBAction func resetTimer(_ sender: AnyObject) {
+    @IBAction func resetTimer(_ sender: Any) {
         startButton.isEnabled = true
         pauseButton.isEnabled = false
         
@@ -45,8 +42,6 @@ class ViewController: UIViewController {
         counter = 0.0
         timeLabel.text = String(counter)
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -54,11 +49,10 @@ class ViewController: UIViewController {
         pauseButton.isEnabled = false
     }
     
-    func UpdateTimer() {
+    @objc func UpdateTimer() {
         counter = counter + 0.1
         timeLabel.text = String(format: "%.1f", counter)
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
