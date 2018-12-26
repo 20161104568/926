217ea3c695c905370c5671c9c1ba658f57985093
20161104568 <a20161104568@Mac02.local>
2018-12-26 09:46:04 +0800

    计时器

--- 926/ViewController.swift
+++ 926/ViewController.swift
@@ -5,37 +5,65 @@
 //  Created by 20161104568 on 2018/9/26.
 //  Copyright © 2018年 20161104568. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var zyjX: UITextField!
-    @IBOutlet weak var zyjY: UITextField!
-    @IBOutlet weak var zyjZ: UITextField!
-    @IBOutlet weak var zyjA: UITextField!
-    @IBOutlet weak var zyjB: UITextField!
-    @IBOutlet weak var zyjC: UITextField!
     
-    @IBAction func test(_ sender: Any) {
-        zyjZ.text = "\(Int(zyjX.text!)! + Int(zyjY.text!)!)"
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    
+    @IBAction func startTimer(_ sender: AnyObject) {
+        if(isPlaying) {
+            return
+        }
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
     }
     
-    @IBAction func test1(_ sender: Any) {
-        zyjA.text = "\(Int(zyjX.text!)! - Int(zyjY.text!)!)"
+    @IBAction func pauseTimer(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
     }
     
-    @IBAction func test2(_ sender: Any) {
-        zyjB.text = "\(Int(zyjX.text!)! * Int(zyjY.text!)!)"
+    @IBAction func resetTimer(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
     }
     
-    @IBAction func test3(_ sender: Any) {
-        zyjC.text = "\(Float(zyjX.text!)! / Float(zyjY.text!)!)"
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = String(counter)
+        pauseButton.isEnabled = false
     }
+    
+    func UpdateTimer() {
+        counter = counter + 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
-
 
